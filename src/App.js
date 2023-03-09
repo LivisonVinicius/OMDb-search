05b852e064514c7bc2f23b28c90feb799fe58455
LivisonVinicius <livisongc@gmail.com>
2023-03-09 20:51:41 -0300

    Header: added id root to App component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Logo from "./assets/images/imdb-logo.png";
 
 function App() {
   return (
-    <>
+    <div id="root">
       <ShellBar
         logo={<img alt="IMDb Logo" src={Logo} />}
         primaryTitle="IMDb search"
         className="center"
       />
-    </>
+    </div>
   );
 }
 
