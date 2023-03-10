4cf595c9b057de034898ec33fa5fe796d51fae94
LivisonVinicius <livisongc@gmail.com>
2023-03-10 03:05:43 -0300

    Style:bootstrap

--- src/Components/AppIntroduction/AppIntroduction.jsx
+++ src/Components/AppIntroduction/AppIntroduction.jsx
@@ -4,7 +4,20 @@ import { Text, Title } from "@ui5/webcomponents-react";
 function AppIntroduction() {
   return (
     <div>
-      <Title className="title">Buscador de filmes</Title>
+      <Title
+        className="title"
+        style={{
+          color: "black",
+          "font-size": "24px",
+          "font-family": "Arial, Helvetica, sans-serif",
+          "font-weight": "bold",
+          "margin-top": "30px",
+          "margin-bottom": "15px",
+          "text-align": "center",
+        }}
+      >
+        Buscador de filmes
+      </Title>
       <Text>Digite no na barra de pesquisa o título de filme desejado.</Text>
     </div>
   );
