a22b48079adda046b704ef011c6339499c7a87fb
LivisonVinicius <livisongc@gmail.com>
2023-03-10 07:38:55 -0300

    Initia main content css

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -1,4 +1,4 @@
-import { Title, Icon } from "@ui5/webcomponents-react";
+import { Title, Icon, RatingIndicator } from "@ui5/webcomponents-react";
 import React from "react";
 import "./MainContent.css";
 
@@ -9,7 +9,16 @@ function MainContent() {
         <Title>Titulo</Title>
         <Icon name="heart-2" style={{ color: "red", "font-size": "16px" }} />
       </div>
-      <div></div>
+      <div className="content">
+        <div className="leftContent">
+          <img src="" alt="Poster" />
+          <RatingIndicator value={0} style="font-size: 28px" />
+        </div>
+        <div className="rightContent">
+            <p>Actor: <span> </span></p>
+            <p>Sinopse:<span> </span></p>
+        </div>
+      </div>
     </div>
   );
 }
