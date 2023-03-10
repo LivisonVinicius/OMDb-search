d9b76d640b9c7cf881799a162ee6fd5da1cdda5c
LivisonVinicius <livisongc@gmail.com>
2023-03-10 00:54:14 -0300

    AppIntroduction:Basic structure

---
+++ src/Components/AppIntroduction/AppIntroduction.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, Title } from "@ui5/webcomponents-react";
+
+function AppIntroduction() {
+  return (
+    <div>
+      <Title>Buscador de filmes</Title>
+      <Text></Text>
+    </div>
+  );
+}
+
+export default AppIntroduction;
