b7f3b37a6fe5c8747e1b2703333549241877b6c0
LivisonVinicius <livisongc@gmail.com>
2023-03-09 22:00:17 -0300

    Header: changes on folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { ShellBar } from "@ui5/webcomponents-react";
 import "./App.scss";
 import Logo from "./assets/images/imdb-logo.png";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div id="root">
-      <ShellBar
-        logo={<img alt="IMDb Logo" src={Logo} />}
-        primaryTitle="IMDb search"
-        className="center"
-      />
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/Navbar.css";
+import Logo from "../../assets/images/imdb-logo.png";
+
+function Navbar() {
+  return (
+    <>
+      <ShellBar
+        logo={<img alt="IMDb Logo" src={Logo} />}
+        primaryTitle="IMDb search"
+        className="center"
+      />
+    </>
+  );
+}
+
+export default Navbar;
